0353aad34900cbfa729d896cb6c4d7aab0f13cb6
jhordy12345 <136488834+jhordy12345@users.noreply.github.com>
2025-11-14 23:10:25 -0500

    Ordena pedidos por fecha descendente

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -7,7 +7,7 @@ class PedidosModel extends Query{
     }
     public function getPedidos($proceso)
     {
-        $sql = "SELECT p.id, p.id_transaccion, p.monto, p.estado, p.fecha, p.direccion AS direccion_pedido, p.ciudad, p.id_cliente, p.proceso, p.id_usuario, c.nombre AS cliente, c.correo AS correo, c.direccion AS direccion, u.nombres AS usuario FROM pedidos p INNER JOIN clientes c ON p.id_cliente = c.id LEFT JOIN usuarios u ON p.id_usuario = u.id WHERE p.proceso = $proceso";
+        $sql = "SELECT p.id, p.id_transaccion, p.monto, p.estado, p.fecha, p.direccion AS direccion_pedido, p.ciudad, p.id_cliente, p.proceso, p.id_usuario, c.nombre AS cliente, c.correo AS correo, c.direccion AS direccion, u.nombres AS usuario FROM pedidos p INNER JOIN clientes c ON p.id_cliente = c.id LEFT JOIN usuarios u ON p.id_usuario = u.id WHERE p.proceso = $proceso ORDER BY p.fecha DESC";
         return $this->selectAll($sql);
     }
     public function actualizarEstado($proceso, $idPedido)
