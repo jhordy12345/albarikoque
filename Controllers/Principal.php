2deab34514ef98c0318cf44df8f8db6c82539cda
jhordy12345 <136488834+jhordy12345@users.noreply.github.com>
2025-11-18 12:52:30 -0500

    Use soles currency for cart

--- Controllers/Principal.php
+++ Controllers/Principal.php
@@ -29,6 +29,7 @@ class Principal extends Controller
         $array['total'] = number_format($total, 2);
         $array['totalPaypal'] = number_format($total, 2, '.', '');
         $array['moneda'] = MONEDA;
+        $array['monedaCodigo'] = MONEDA_CODE;
         echo json_encode($array, JSON_UNESCAPED_UNICODE);
         die();
     }
