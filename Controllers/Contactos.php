ae65a0807b3250249867f6a613b4dc97536c80a9
jhordy12345 <136488834+jhordy12345@users.noreply.github.com>
2025-11-19 15:13:34 -0500

    Remove redundant PHPMailer includes

--- Controllers/Contactos.php
+++ Controllers/Contactos.php
@@ -1,10 +1,7 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
-require 'vendor/autoload.php';
 
 class Contactos extends Controller
 {
